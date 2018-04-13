a7dea95f0600890f63239aa08307c538cc4aa798
ClaytonWong <c_w_serious-github@yahoo.com.au>
2018-04-13 17:27:17 +1000

    Add files
    Include.rb and Gsub.rb added.

---
+++ Gsub.rb
@@ -0,0 +1,4 @@
+sample_string = 'car1'
+puts sample_string
+sample_string.gsub!(/1/, '2') # Change the 1 in car1 to a 2
+puts sample_string
---
+++ Include.rb
@@ -0,0 +1,6 @@
+sample_string = 'goodbye'
+if sample_string.include? "h"
+    puts "sample_string has a 'h'"
+else
+    puts "sample_string has no 'h'"
+end
